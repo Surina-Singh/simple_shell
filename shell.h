081a4b8dff9dd2ad388fe9526085b0374bb2f351
Bex <ruthrebekahcupido@gmail.com>
2022-08-03 15:49:00 -0700

    Header file

---
+++ shell.h
@@ -0,0 +1,16 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/stat.h>
+#include <errno.h>
+
+int _putchar(char c);
+int main(int ac, char **av);
+
+#endif
