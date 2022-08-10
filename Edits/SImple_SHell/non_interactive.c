792a91e1d914fea94ad23c844a8ba6466c466106
Bex <ruthrebekahcupido@gmail.com>
2022-08-09 22:39:43 -0700

    File updates

---
+++ Edits/SImple_SHell/non_interactive.c
@@ -0,0 +1,53 @@
+#include "shell.h"
+/**
+ * c_ignore - custom ignores spaces and newlines
+ * @str: envrionmental variables
+ * Return: new string
+ */
+char *c_ignore(char *str)
+{
+	while (*str == ' ' || *str == '\n')
+		str++;
+	return (str);
+}
+
+/**
+ * non_interactive - handles when user pipes commands into shell via pipeline
+ * @env: envrionmental variables
+ */
+void non_interactive(list_t *env)
+{
+	size_t i = 0, n = 0;
+	int command_line_no = 0, exit_stat = 0;
+	char *command = NULL, *n_command = NULL, **n_line, **token;
+
+	i = get_buffer(&command);
+	if (i == 0)
+	{
+		free(command);
+		exit(0);
+	}
+	n_command = command;
+	command = c_ignore(command);
+	n_line = cstr_tok(command, "\n");
+	if (n_command != NULL)
+		free(n_command);
+	n = 0;
+	while (n_line[n] != NULL)
+	{
+		command_line_no++;
+		token = NULL;
+		token = cstr_tok(n_line[n], " ");
+		exit_stat = built_in(token, env, command_line_no, n_line);
+		if (exit_stat)
+		{
+			n++;
+			continue;
+		}
+		exit_stat = execve_cmd(token, env, command_line_no);
+		n++;
+	}
+	free_double_ptr(n_line);
+	free_linked_list(env);
+	exit(exit_stat);
+}
---
+++ Edits/SImple_SHell/which_cmd.c
@@ -0,0 +1,36 @@
+#include "shell.h"
+/**
+ * which_cmd - fleshes out command by appending it to a matching PATH directory
+ * @str: first command user typed into shell
+ * @env: environmental variable
+ * Return: a copy of fleshed out command
+ */
+char *which_cmd(char *str, list_t *env)
+{
+	char *pth, *cat = NULL, **toks;
+	int i = 0;
+
+	pth = get_env("PATH", env);
+	toks = cstr_tok(pth, ":");
+	free(pth);
+
+	i = 0;
+	while (toks[i] != NULL)
+	{
+		if (toks[i][0] == '\0')
+			cat = getcwd(cat, 0);
+		else
+			cat = strdup(toks[i]);
+		cat = strcat(cat, "/");
+		cat = strcat(cat, str);
+		if (access(cat, F_OK) == 0)
+		{
+			free_double_ptr(toks);
+			return (cat);
+		}
+		free(cat);
+		i++;
+	}
+	free_double_ptr(toks);
+	return (str);
+}
