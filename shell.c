aa0413b44a85ceefa9f3aa5e5e5851222104d766
Surina-Singh <surinsingh@hotmail.co.za>
2022-08-10 04:46:51 -0700

    update shell.c

--- shell.c
+++ shell.c
@@ -5,7 +5,7 @@ unsigned int sig_flag;
 
 /**
  * sig_handler - handles ^C signal interupt
- * @uuv: unused variable (required for signal function prototype)
+ * @uuv: unused variable
  *
  * Return: void
  */
@@ -13,16 +13,18 @@ static void sig_handler(int uuv)
 {
 	(void) uuv;
 	if (sig_flag == 0)
+
 		_puts("\n$ ");
+
 	else
 		_puts("\n");
 }
 
 /**
- * main - main function for the shell
- * @argc: number of arguments passed to main
- * @argv: array of arguments passed to main
- * @environment: array of environment variables
+ * main - shell main function
+ * @argc: argument numbers passed to main
+ * @argv: argument arrays passed to main
+ * @environment: environment variables
  *
  * Return: 0 or exit status, or ?
  */
@@ -35,19 +37,24 @@ int main(int argc __attribute__((unused)), char **argv, char **environment)
 	vars.argv = argv;
 	vars.env = make_env(environment);
 	signal(SIGINT, sig_handler);
+
 	if (!isatty(STDIN_FILENO))
 		is_pipe = 1;
+
 	if (is_pipe == 0)
 		_puts("$ ");
 	sig_flag = 0;
+
 	while (getline(&(vars.buffer), &len_buffer, stdin) != -1)
 	{
 		sig_flag = 1;
 		vars.count++;
 		vars.commands = tokenize(vars.buffer, ";");
+
 		for (i = 0; vars.commands && vars.commands[i] != NULL; i++)
 		{
 			vars.av = tokenize(vars.commands[i], "\n \t\r");
+
 			if (vars.av && vars.av[0])
 				if (check_for_builtins(&vars) == NULL)
 					check_for_path(&vars);
@@ -56,10 +63,12 @@ int main(int argc __attribute__((unused)), char **argv, char **environment)
 		free(vars.buffer);
 		free(vars.commands);
 		sig_flag = 0;
+
 		if (is_pipe == 0)
 			_puts("$ ");
 		vars.buffer = NULL;
 	}
+
 	if (is_pipe == 0)
 		_puts("\n");
 	free_env(vars.env);
