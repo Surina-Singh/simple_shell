0301ca2e258c962ec99209a9ccd7193061515a73
Surina-Singh <surinsingh@hotmail.co.za>
2022-08-10 07:58:30 -0700

    update new_strtok.c

--- new_strtok.c
+++ new_strtok.c
@@ -1,9 +1,9 @@
 #include "shell.h"
 
 /**
- * check_match - checks if a character matches any in a string
- * @c: character to check
- * @str: string to check
+ * check_match - checks for matches of character to string
+ * @c: character
+ * @str: string
  *
  * Return: 1 if match, 0 if not
  */
@@ -18,51 +18,4 @@ unsigned int check_match(char c, const char *str)
 			return (1);
 	}
 	return (0);
-}
-
-/**
- * new_strtok - custom strtok
- * @str: string to tokenize
- * @delim: delimiter to tokenize against
- *
- * Return: pointer to the next token or NULL
- */
-char *new_strtok(char *str, const char *delim)
-{
-	static char *token_start;
-	static char *next_token;
-	unsigned int i;
-
-	if (str != NULL)
-		next_token = str;
-	token_start = next_token;
-	if (token_start == NULL)
-		return (NULL);
-	for (i = 0; next_token[i] != '\0'; i++)
-	{
-		if (check_match(next_token[i], delim) == 0)
-			break;
-	}
-	if (next_token[i] == '\0' || next_token[i] == '#')
-	{
-		next_token = NULL;
-		return (NULL);
-	}
-	token_start = next_token + i;
-	next_token = token_start;
-	for (i = 0; next_token[i] != '\0'; i++)
-	{
-		if (check_match(next_token[i], delim) == 1)
-			break;
-	}
-	if (next_token[i] == '\0')
-		next_token = NULL;
-	else
-	{
-		next_token[i] = '\0';
-		next_token = next_token + i + 1;
-		if (*next_token == '\0')
-			next_token = NULL;
-	}
-	return (token_start);
 }
